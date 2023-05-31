d99a6f4ef040c5489913cd899afdcf7dfd739eb6
Abulkalam <abulkalamasif1129@gmail.com>
2023-05-31 21:08:44 +0500

    Video 56 completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
