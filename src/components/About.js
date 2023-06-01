e205c3aa506747ee3aa5019d9e008e226af3a2ea
Abulkalam <abulkalamasif1129@gmail.com>
2023-06-01 18:44:16 +0500

    Video 58 completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import NoteContext from '../context/notes/NoteContext';
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      This is about
+    </>
   )
 }
 
